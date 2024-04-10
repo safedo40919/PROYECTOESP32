4d226240db1bb5f7f3bbc76d1fb09de4fd4711b9
alastalord <alastar346@gmail.com>
2024-04-10 00:14:45 -0400

    feat: connet to api

--- arduino/main.ino
+++ arduino/main.ino
@@ -3,18 +3,20 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid  = "Dios es bello";
-const char* password  = "Rgm84074065";
+const char *ssid = "Dios es bello";
+const char *password = "Rgm84074065";
 const int pinSensor = 16; // Faltaba un punto y coma aquí
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-void msg(String message) { // Se agregó el tipo de retorno 'void' antes del nombre de la función
+void msg(String message)
+{ // Se agregó el tipo de retorno 'void' antes del nombre de la función
   lcd.print(message);
   delay(1000);
   lcd.clear();
 }
 
-void setup() {
+void setup()
+{
 
   Serial.begin(9600);
 
@@ -24,23 +26,34 @@ void setup() {
 
   // Conectar a la red WiFi
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-   msg("error wifi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    msg("error wifi");
   }
 
   // Mensaje de la conexión de wifi en pantalla
   msg("coneccion de wifi estable");
 }
 
+void loop()
+{
+  int sensor = digitalRead(pinSensor);
+  HTTPClient res;
 
-void loop() {
- int sensor = digitalRead(pinSensor);
+  if (sensor == HIGH)
+  {
+    res.begin(String("127.0.0.1:3000/user"));
 
- if (sensor == HIGH) {
-    msg("Movimiento Detectado");         
+    int statuscode = res.get();
+
+    if (statuscode > 0)
+    {
+      String data = res.getString();
+      msg(data);
+    }
   }
-  else{
+  else
+  {
     msg("No hay movimiento");
   }
-  
 }
