d511735fbfc1262ddbef6267721bc0bba82b6578
safedo40919 <jsgamez28@gmail.com>
2024-04-09 22:24:37 -0500

    codigo de arduino

---
+++ arduino/main.ino
@@ -0,0 +1,46 @@
+#include <WiFi.h>
+#include "LiquidCrystal_I2C.h"
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid  = "Dios es bello";
+const char* password  = "Rgm84074065";
+const int pinSensor = 16; // Faltaba un punto y coma aquí
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void msg(String message) { // Se agregó el tipo de retorno 'void' antes del nombre de la función
+  lcd.print(message);
+  delay(1000);
+  lcd.clear();
+}
+
+void setup() {
+
+  Serial.begin(9600);
+
+  // Iniciando lcd
+  lcd.init();
+  lcd.backlight();
+
+  // Conectar a la red WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+   msg("error wifi");
+  }
+
+  // Mensaje de la conexión de wifi en pantalla
+  msg("coneccion de wifi estable");
+}
+
+
+void loop() {
+ int sensor = digitalRead(pinSensor);
+
+ if (sensor == HIGH) {
+    msg("Movimiento Detectado");         
+  }
+  else{
+    msg("No hay movimiento");
+  }
+  
+}
